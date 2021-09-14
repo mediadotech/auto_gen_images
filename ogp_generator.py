57150e4691e74055d50d6b62041094750854eb87
Phung Van Buu <buuphung@offshore-it.biz>
2021-09-14 16:56:46 +0700

    basic generate ticket layout from svg template

---
+++ ogp_generator.py
@@ -0,0 +1,33 @@
+# 実際のAPIはもっとちゃんと作ってます（イメージを掴むためのサンプルです）
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# さっきのOGP生成関数
+from sample_image import convert_ogp
+# import upload
+
+
+class OgpContext(BaseModel):
+    age: int
+    area: str
+    period: str
+
+# docは公開する必要ない（しちゃ🙅）なので使いません
+app = FastAPI(docs_url=None, redoc_url=None)
+
+
+@app.post('/create')
+def create(title: str, content: str, start_date: str, start_time: str, organizer_name: str, is_greeter: bool, color: str):
+    image_byte = convert_ogp(title, content, start_date, start_time, organizer_name, is_greeter, color, 'tem.svg')
+    # print(age)
+    # image_byte = convert_ogp(form.area, form.age, form.period, 'templates/ogp.svg.j2')    
+    # 取得したimageを何かしらの方法でアップロード
+    # upload(image_byte)
+    return {'status': 'ok'}
+
+
+if __name__ == '__main__':
+    import os
+    import uvicorn
+
+    uvicorn.run(app, host='0.0.0.0', port=os.getenv('PORT', 3000))
