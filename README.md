69e6a72828bd75d72828c09e8cdb82de61e65c90
Phung Van Buu <buuphung@offshore-it.biz>
2021-10-03 16:39:00 +0700

    tidy up source and update README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-greet_ticket_thumbnail_generator
+
+# Run sample in docker
+
+## Build image using Dockerfile
+```bash
+$ docker build -t your_image_name .
+```
+
+## Run the image
+
+```bash
+$ docker run -d --name your_container_name -p 3000:3000 your_image_name
+```
--- app/main.py
+++ app/main.py
@@ -1,34 +1,27 @@
-# 実際のAPIはもっとちゃんと作ってます（イメージを掴むためのサンプルです）
 from fastapi import FastAPI
-
 import cairosvg
 from jinja2 import Environment, FileSystemLoader
 import tempfile
 import os
-# さっきのOGP生成関数
 
-# docは公開する必要ない（しちゃ🙅）なので使いません
 app = FastAPI(docs_url=None, redoc_url=None)
-
 
 @app.post('/create')
 def create(title: str, content: str, start_date: str, start_time: str, organizer_name: str, is_greeter: bool, color: str):
-    image_byte = convert_ogp(title, content, start_date, start_time, organizer_name, is_greeter, color, 'template/ticket.svg')
-    # print(age)
-    # image_byte = convert_ogp(form.area, form.age, form.period, 'templates/ogp.svg.j2')    
-    # 取得したimageを何かしらの方法でアップロード
-    # upload(image_byte)
+    image = convert_ogp(title, content, start_date, start_time, organizer_name, is_greeter, color, 'template/ticket.svg')
+    # WIP: upload image file to S3 with above image
     return {'status': 'ok'}
 
 
 def convert_ogp(title: str, content: str, start_date: str, start_time: str, organizer_name: str, is_greeter: bool, color: str, template_file: str):
-    # テンプレファイルと引数からテキストを生成
+    # Setup default environment param
     env = Environment(loader=FileSystemLoader(os.path.dirname(template_file)))
     ogp_template = env.get_template(os.path.basename(template_file))
-    ogp_context = ogp_template.render(title=title, content=content, start_d=start_date, start_t=start_time, name=organizer_name, condition=is_greeter, color=color)
-    # ogp_context = '<svg xmlns="http://www.w3.org/2000/svg" width="965" height="430" viewBox="0 0 965 430"><rect width="965" height="430" rx="20" fill="#dcdce6"/></svg>'
 
-   # TemporaryFileとして書き出して後byteでもらう
+    # Replace template with desired data
+    ogp_context = ogp_template.render(title=title, content=content, start_d=start_date, start_t=start_time, name=organizer_name, condition=is_greeter, color=color)
+
+   # Write temporary file and get image bytes later
     with tempfile.NamedTemporaryFile('w') as f:
         f.write(ogp_context)
         f.flush()
